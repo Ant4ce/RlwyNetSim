6cf00c1ed7b670c835084e9ebf4aae6b87ad5949
fbachus <felix.bachstein@gmail.com>
2023-02-20 11:14:32 +0100

    remove start_station and end_station from route.rs

--- src/line/route.rs
+++ src/line/route.rs
@@ -2,24 +2,15 @@
 pub struct Route {
     pub id: u32,
     pub name: String,
-    pub start_station: u32,
-    pub end_station: u32,
 }
 
 impl Route {
-    pub fn new(
-        id: &mut u32,
-        name: String,
-        start_station: u32,
-        end_station: u32) -> Route {
-
+    pub fn new(id: &mut u32,name: String) -> Route {
         *id += 1;
-        
+
         Route{
             id: id.clone(),
             name,
-            start_station,
-            end_station,
         }
     }
 }
@@ -30,8 +21,8 @@ mod tests {
 
     #[test]
     fn adding_route() {
-        let test_route = Route::new(&mut 0, String::from("Geneva"), 2, 3);
-        assert_eq!((test_route.id, test_route.name, test_route.start_station, test_route.end_station),
-                   (1, String::from("Geneva"), 2, 3));
+        let test_route = Route::new(&mut 0, String::from("Geneva"));
+        assert_eq!((test_route.id, test_route.name),
+                   (1, String::from("Geneva")));
     }
 }
