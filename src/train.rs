8052a845860283ea6ebd257cac0111647392ee9a
fbachus <felix.bachstein@gmail.com>
2023-02-22 19:15:21 +0100

    WIP dont push

--- src/train.rs
+++ src/train.rs
@@ -1,22 +1,24 @@
 use std::collections::HashMap;
-use petgraph::stable_graph::{NodeIndex, EdgeIndex};
+use std::sync::{Arc, Mutex};
+use petgraph::stable_graph::{NodeIndex, EdgeIndex, EdgeReference};
+use petgraph::visit::EdgeRef;
 use crate::station::Station;
 use crate::route::Route;
 
 #[derive(Debug, PartialEq)]
-pub struct Train<T> {
+pub struct Train {
     id: u32,
     train_type: TrainType,
-    location: Location<T>,
+    location: Location,
     route_name: String,
     dir_forward: bool,
     model: String,
 }
 
-pub struct TrainRegister<T> {
+pub struct TrainRegister {
     name: String,
     next_train_id: u32,
-    train_list: Vec<Train<T>>
+    train_list: Vec<Train>
 }
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
@@ -26,9 +28,9 @@ pub enum TrainType {
     HighSpeed,
 }
 
-impl<T> Train<T> {
-    fn new(id: &mut u32, train_type: TrainType, location: Location<T>,
-               route_name: String, dir_forward: bool, model: String) -> Train<T> {
+impl Train {
+    fn new(id: &mut u32, train_type: TrainType, location: Location,
+               route_name: String, dir_forward: bool, model: String) -> Train {
 
         let new_train = Train {
             id: id.clone(),
@@ -41,18 +43,40 @@ impl<T> Train<T> {
         *id += 1;
         new_train
     }
+    fn move_forward(&mut self, graph: &petgraph::stable_graph::StableGraph
+    <Arc<Mutex<Station>>, Arc<Mutex<Route>>>) {
+        match &self.location {
+            Location::NodeTypeIndex(value) => {let filtered_routes = graph.edges(*value).filter(|x|
+                x.weight().lock().unwrap().name == self.route_name);
+                for route in filtered_routes {
+                    let unwrapped_route = route.weight().lock().unwrap().clone();
+                    let suffix_forward = unwrapped_route.name.chars().last().unwrap() == 'f';
+                    if self.dir_forward == suffix_forward {
+                        self.location = Location::EdgeTypeIndex(route.id());
+                    } else {
+                        continue
+                    }
+                }
+            },
+            Location::EdgeTypeIndex(value) => {let filtered_edge = graph.edge_endpoints(*value).unwrap().1;
+                self.location = Location::NodeTypeIndex(filtered_edge);
+            }
+            _ => panic!("TrainLocation is neither NodeIndex nor EdgeIndex")
+        };
+
+    }
 
 }
 
-impl<T> TrainRegister<T> {
-    pub fn new(register_name: String) -> TrainRegister<T> {
+impl TrainRegister {
+    pub fn new(register_name: String) -> TrainRegister {
         TrainRegister {
             name: register_name,
             next_train_id: 0 as u32,
             train_list: vec![]
         }
     }
-    pub fn add_train(&mut self, train_type: TrainType, location: Location<T>,
+    pub fn add_train(&mut self, train_type: TrainType, location: Location,
                      route_name: String, dir_forward: bool, model: String) -> u32 {
         let new_train = Train::new(&mut self.next_train_id, train_type, location,
                                    route_name, dir_forward, model);
@@ -62,12 +86,26 @@ impl<T> TrainRegister<T> {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Location<T> {
-    index: T,
+pub enum Location {
+    NodeTypeIndex(NodeIndex),
+    EdgeTypeIndex(EdgeIndex)
 }
 
-impl<T> Location<T> {
-    pub fn get_location(&self) -> &T{
-        &self.index
+#[cfg(test)]
+mod tests {
+    use petgraph::prelude::StableGraph;
+    use super::*;
+    use crate::graph;
+
+    fn construct_scenario(fake_id: &mut u32) -> StableGraph<Arc<Mutex<Station>>, Arc<Mutex<Route>>> {
+        let mut test_graph =
+            StableGraph::<Arc<Mutex<Station>>, Arc<Mutex<Route>>>::new();
+        let station1 = test_graph.add_station_to_graph(&mut test_graph, fake_id,
+                                       String::from("Berlin"), vec![(1, TrainType::LowSpeed)]);
+    }
+
+    #[test]
+    fn moving_train_from_node() {
+
     }
 }
