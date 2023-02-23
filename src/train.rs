70876b5d360500e7c9f4fe3157f371eead9979f4
fbachus <felix.bachstein@gmail.com>
2023-02-23 01:53:54 +0100

    rewrite train moving functionality
    reduce code redundancy, use iterators and functions instead of multiple loops

--- src/train.rs
+++ src/train.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use petgraph::stable_graph::{NodeIndex, EdgeIndex, EdgeReference};
 use petgraph::visit::EdgeRef;
@@ -45,61 +44,69 @@ impl Train {
     }
     pub fn move_forward(&mut self, graph: &petgraph::stable_graph::StableGraph
     <Arc<Mutex<Station>>, Arc<Mutex<Route>>>) {
-        let (mut route_forward_name, mut route_backward_name) = (self.route_name.clone(), self.route_name.clone());
+        let (mut route_forward_name, mut route_backward_name) =
+            (self.route_name.clone(), self.route_name.clone());
         route_forward_name.push('f');
         route_backward_name.push('b');
-        match &self.location {
+        match self.location {
             Location::NodeTypeIndex(value) => {
-                println!("{:?}", graph.edges(*value));
-                println!("{:?}", route_forward_name);
-                println!("{:?}", route_backward_name);
-                let filtered_route_1 = graph.edges(*value).filter(|x|
-                    x.weight().lock().unwrap().name == route_forward_name);
-                let filtered_route_2 = graph.edges(*value).filter(|x|
-                        x.weight().lock().unwrap().name == route_backward_name);
-                let filtered_route_3 = graph.edges(*value).filter(|x|
-                    x.weight().lock().unwrap().name == route_forward_name);
-                for route in filtered_route_1 {
-                    let unwrapped_route = route.weight().lock().unwrap().clone();
-                    let suffix_forward = unwrapped_route.name.chars().last().unwrap() == 'f';
-                    if self.dir_forward == suffix_forward {
-                        self.location = Location::EdgeTypeIndex(route.id());
-                        return
-                    } else {
-                        continue
-                    }
-                };
-                self.dir_forward = false;
-                for route in filtered_route_2 {
-                    let unwrapped_route = route.weight().lock().unwrap().clone();
-                    let suffix_forward = unwrapped_route.name.chars().last().unwrap() == 'f';
-                    if self.dir_forward == suffix_forward {
-                        self.location = Location::EdgeTypeIndex(route.id());
-                        return
-                    } else {
-                        continue
-                    }
-                };
-                self.dir_forward = true;
-                for route in filtered_route_3 {
-                    let unwrapped_route = route.weight().lock().unwrap().clone();
-                    let suffix_forward = unwrapped_route.name.chars().last().unwrap() == 'f';
-                    if self.dir_forward == suffix_forward {
-                        self.location = Location::EdgeTypeIndex(route.id());
-                        return
-                    } else {
-                        continue
-                    }
-                };
+                self.change_train_location(graph, &value,
+                                     &route_forward_name, &route_backward_name, false)
             },
-            Location::EdgeTypeIndex(value) => {let filtered_edge = graph.edge_endpoints(*value).unwrap().1;
+            Location::EdgeTypeIndex(value) => {
+                let filtered_edge =
+                    graph.edge_endpoints(value).unwrap().1;
                 self.location = Location::NodeTypeIndex(filtered_edge);
             }
             _ => panic!("TrainLocation is neither NodeIndex nor EdgeIndex")
         };
 
     }
+    fn change_train_location(&mut self,
+                             current_graph: &petgraph::stable_graph::
+                                StableGraph<Arc<Mutex<Station>>, Arc<Mutex<Route>>>,
+                             current_location: &NodeIndex,
+                             route_forward_name: &String,
+                             route_backward_name: &String,
+                             called_recurs: bool) {
+        current_graph.edges(*current_location)
+            .filter(|y| compare_route_names(
+                &y.weight().lock().unwrap().name,
+                &route_forward_name, &route_backward_name))
+            .for_each(|y| {
+                if self.dir_forward &&
+                    &y.weight().lock().unwrap().name.chars().last().unwrap() == &'f' {
+                    self.location = Location::EdgeTypeIndex((y.id()));
+                } else if self.dir_forward == false &&
+                    &y.weight().lock().unwrap().name.chars().last().unwrap() == &'b' {
+                    self.location = Location::EdgeTypeIndex((y.id()));
+                }
+            });
+        if self.location != Location::NodeTypeIndex(*current_location) { return }
+        if self.dir_forward { self.dir_forward = false }
+        else { self.dir_forward = true }
+        if called_recurs { panic!("cannot move Train forward in either direction!") }
+        self.change_train_location(current_graph, current_location,
+                                   route_forward_name, route_backward_name, true)
+    }
 
+}
+/// Compares forward- and backward variations of route names in Train and compares them to the route name in the graph
+///
+/// # Examples
+/// ```
+/// let (name_in_graph, route_name_fo, route_name_ba) =
+///     String::from("BerlinPotsdamf"),
+///     String::from("BerlinPotsdamf"),
+///     String::from("BerlinPotsdamb");
+/// asserteq!(true, compare_route_names(name_in_graph, route_name_fo, route_name_ba)
+/// ```
+fn compare_route_names(name_in_graph: &String,
+                       route_name_forward_direction: &String,
+                       route_name_back_direction: &String) -> bool{
+    if name_in_graph == route_name_forward_direction ||
+        name_in_graph == route_name_back_direction { true }
+    else { false }
 }
 
 impl TrainRegister {
@@ -130,23 +137,25 @@ mod tests {
     use petgraph::stable_graph::StableGraph;
     use super::*;
     use crate::graph;
-    use crate::train::Location;
-    use crate::train::Location::EdgeTypeIndex;
+    use crate::train::Location::{EdgeTypeIndex, NodeTypeIndex};
 
     fn construct_scenario(fake_id: &mut u32) -> (StableGraph<Arc<Mutex<Station>>, Arc<Mutex<Route>>>,
                                                  NodeIndex, NodeIndex, NodeIndex) {
         let mut test_graph =
             StableGraph::<Arc<Mutex<Station>>, Arc<Mutex<Route>>>::new();
         let station1 = graph::add_station_to_graph(&mut test_graph, fake_id,
-                           String::from("Tokyo"), vec![(8, TrainType::LowSpeed),(2, TrainType::HighSpeed)]);
+                           String::from("Tokyo"), vec![(8, TrainType::LowSpeed),
+                                                       (2, TrainType::HighSpeed)]);
         let station2 = graph::add_station_to_graph(&mut test_graph, fake_id,
-                           String::from("Nagoya"), vec![(3, TrainType::LowSpeed),(1, TrainType::HighSpeed)]);
+                           String::from("Nagoya"), vec![(3, TrainType::LowSpeed),
+                                                        (1, TrainType::HighSpeed)]);
         let station3 = graph::add_station_to_graph(&mut test_graph, fake_id,
-                           String::from("Osaka"), vec![(5, TrainType::LowSpeed),(2, TrainType::HighSpeed)]);
-        graph::add_route_to_graph(&mut test_graph, station1, station2,
-                                  fake_id, String::from("Tokaido_Shinkansen"), true);
-        graph::add_route_to_graph(&mut test_graph, station2, station3,
-                                  fake_id, String::from("Tokaido_Shinkansen"), true);
+                           String::from("Osaka"), vec![(5, TrainType::LowSpeed),
+                                                       (2, TrainType::HighSpeed)]);
+        graph::add_route_to_graph(&mut test_graph, station1, station2, fake_id,
+                                  String::from("Tokaido_Shinkansen"), true);
+        graph::add_route_to_graph(&mut test_graph, station2, station3, fake_id,
+                                  String::from("Tokaido_Shinkansen"), true);
         (test_graph, station1, station2, station3)
     }
 
@@ -161,18 +170,19 @@ mod tests {
                              String::from("Shinkansen"));
         println!("Starting at NodeIndex(0)");
         test_fleet.train_list[0].move_forward(&test_graph);
-        assert_eq!(test_fleet.train_list[0].location, EdgeTypeIndex(test_graph.find_edge(station1, station2).unwrap()));
+        assert_eq!(test_fleet.train_list[0].location,
+                   EdgeTypeIndex(test_graph.find_edge(station1, station2).unwrap()));
+        for _ in 0..3 { test_fleet.train_list[0].move_forward(&test_graph); }
+        assert_eq!(test_fleet.train_list[0].location,
+                   NodeTypeIndex(station3));
+        println!("successfully reached endstation, time for a turnaround!");
         test_fleet.train_list[0].move_forward(&test_graph);
-        test_fleet.train_list[0].move_forward(&test_graph);
-        assert_eq!(test_fleet.train_list[0].location, EdgeTypeIndex(test_graph.find_edge(station2, station3).unwrap()));
-        test_fleet.train_list[0].move_forward(&test_graph);
-        test_fleet.train_list[0].move_forward(&test_graph);
+        assert_eq!(test_fleet.train_list[0].location,
+                   EdgeTypeIndex(test_graph.find_edge(station3, station2).unwrap()));
         println!("turnaround backwards worked!");
-        test_fleet.train_list[0].move_forward(&test_graph);
-        test_fleet.train_list[0].move_forward(&test_graph);
-        test_fleet.train_list[0].move_forward(&test_graph);
-        test_fleet.train_list[0].move_forward(&test_graph);
-        assert_eq!(test_fleet.train_list[0].location, EdgeTypeIndex(test_graph.find_edge(station1, station2).unwrap()));
-        println!("turnaround forwards worked!");
+        for _ in 1..5 { test_fleet.train_list[0].move_forward(&test_graph); }
+        assert_eq!(test_fleet.train_list[0].location,
+                   EdgeTypeIndex(test_graph.find_edge(station1, station2).unwrap()));
+        println!("turnaround forwards worked, the train go around infinitely");
     }
 }
