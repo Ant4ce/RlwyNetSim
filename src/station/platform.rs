c3d8162d5899a50cac4381b286771b5e573c0e18
fbachus <felix.bachstein@gmail.com>
2023-03-08 15:20:06 +0100

    implement Display Trait for TrainType, Station and Platform

--- src/station/platform.rs
+++ src/station/platform.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use crate::train::TrainType;
 
 /// Used by Station
@@ -9,7 +10,6 @@ pub struct Platform {
     pub occupied: bool,
     pub platform_type: TrainType,
 }
-
 impl Platform {
     pub fn new(id: u8, platform_type: TrainType) -> Platform {
         
@@ -20,4 +20,20 @@ impl Platform {
             platform_type: platform_type,
         }
     }
+}
+
+const OCCUPIED: &str = "occupied";
+const FREE: &str = "free";
+impl Display for Platform {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let is_occupied;
+        if self.occupied {
+            is_occupied = String::from(OCCUPIED)
+        }
+        else {
+            is_occupied = String::from(FREE);
+        }
+        write!(f, "{:?}, {:?}, {:?}", self.id, self.platform_type, is_occupied)
+    }
+
 }
